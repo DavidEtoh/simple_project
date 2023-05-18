3f683fc1af3ef3457ee01043cb5d80d8ddde83e9
DavidEtoh <Davidetoh295@gmail.com>
2023-05-18 15:37:11 -0700

    simple_project

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies the string
+ * @dest: the destination of the code
+ * @src: the source of the code
+ * Return: pointer of the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - duplicates the string in the code
+ * @str: the duplicate string
+ * Return: pointer of the duplicated string
+ */
+
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints the input of the string
+ * @str: the string that is to be printed
+ * Return: Nothing!
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the char c to the stdout
+ * @c: The char to be printed
+ * Return: if success 1.
+ * if error, -1 to be returned, and errno is to be appropriately set.
+ */
+
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
